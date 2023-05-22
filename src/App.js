f02ae6436f9a94983625fae8803dd1f0acf5700e
m010it <79036865+m010it@users.noreply.github.com>
2023-05-22 09:22:00 +0700

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
 import './style.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-export default function App() {
+const ArticleList = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const response = await axios.get('http://localhost:1337/articles');
+        setArticles(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchArticles();
+  }, []);
+
   return (
     <div>
-      <h1>Hello StackBlitzzzzz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      {articles.map((article) => (
+        <div key={article.id}>
+          <h2>{article.title}</h2>
+          <p>{article.content}</p>
+        </div>
+      ))}
     </div>
   );
-}
+};
+
+export default ArticleList;
